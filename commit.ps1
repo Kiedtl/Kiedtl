f01494324432f655d1148ce50410a69f4e068018
Kiedtl <eli.georgios@gmail.com>
2018-11-07 09:52:25 -0500

    November 7, 2018
    Signed-off-by: Kiedtl <eli.georgios@gmail.com>

---
+++ commit.ps1
@@ -0,0 +1,7 @@
+surge ./ kiedtl.surge.sh
+git add .
+$commitmsg = read-host "Enter a commit message"
+set-content commitmsg.tmp $commitmsg
+git commit -s -F commitmsg.tmp
+pause
+remove-item commitmsg.tmp
