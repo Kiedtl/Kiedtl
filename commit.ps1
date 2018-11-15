a1dfb2611e0fc9e0f8bd17cde68c34a960b4812c
Kiedtl <eli.georgios@gmail.com>
2018-11-15 14:17:50 -0500

    Removed all root content into root directory, and added contnt for 'lots' subdomain into lots directory
    Signed-off-by: Kiedtl <eli.georgios@gmail.com>

--- commit.ps1
+++ commit.ps1
@@ -1,4 +1,3 @@
-surge ./ kiedtl.surge.sh
 git add .
 $commitmsg = read-host "Enter a commit message"
 set-content commitmsg.tmp $commitmsg
@@ -6,3 +5,8 @@ git commit -s -F commitmsg.tmp
 pause
 remove-item commitmsg.tmp
 git push origin master
+
+surge ./root kiedtl.surge.sh
+surge ./lots lots.kiedtl.surge.sh
+pause
+
